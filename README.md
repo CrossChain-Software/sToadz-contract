0656e3dc22b8602f4e17f56aa46a08ff3d86287d
Bradley Woolf <bradleymwoolf@gmail.com>
2022-03-11 10:44:08 -0800

    config for deployment

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Airdropped NFTs for whoever mints sToadz:
 
 - Buildings
 - SongBirdCity
+
+## Testing config
+
+- create `.env` and structure like this:
+
+```
+RINKEBY_INFURA_ENDPOINT=""
+ETHERSCAN_API_KEY=""
+PRIVATE_KEY1=""
+PRIVATE_KEY2=""
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,66 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const RINKEBY_INFURA_ENDPOINT = process.env.RINKEBY_INFURA_ENDPOINT;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const PRIVATE_KEY1 = process.env.PRIVATE_KEY1;
+const PRIVATE_KEY2 = process.env.PRIVATE_KEY2;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.11",
+  defaultNetwork: "localhost",
   networks: {
     localhost: {
-      timeout: 100_000,
+      url: "http://127.0.0.1:8545",
+    },
+    rinkeby: {
+      url: RINKEBY_INFURA_ENDPOINT,
+      accounts: [PRIVATE_KEY1, PRIVATE_KEY2],
+      gas: "auto",
     },
   },
+  solidity: {
+    version: "0.8.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 2000000,
+  },
+  gasReporter: {
+    currency: "usd",
+    gasPrice: 150,
+    coinmarketcap: "1ecfa6fd-b2de-42e2-8ffd-4a2914c358b0",
+  },
 };
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// module.exports = {
+//   solidity: "0.8.11",
+//   networks: {
+//     localhost: {
+//       timeout: 100_000,
+//     },
+//   },
+// };
