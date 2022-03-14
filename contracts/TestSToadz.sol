35cb062fe8e226ab49838cdf9a9b73a937bdd2d9
Bradley Woolf <bradleymwoolf@gmail.com>
2022-03-14 07:52:59 -0700

    move wallets to readme

--- contracts/TestSToadz.sol
+++ contracts/TestSToadz.sol
@@ -72,13 +72,6 @@ contract TestSToadz is LilOwnable, ERC721 {
         require(msg.sender == _owner, "Ownable: caller is not the owner");
         _;
     }
-    
-    /// crosschain- 16.6%
-        /// frontend dev (4.98%)- 30% (0xb20F2a4601aED75B886CC5B84E28a0D65a7Bfd48)
-        /// backend dev (5.81%)- 35% (0x90ca2B438482f2b205dA814B94b4758c3a229541)
-        /// backend dev (5.81%)- 35% (0x8e23A0C18D2Fd631eFA838aCC1DfBecbbdB3ADD9)
-    /// sToadz- 80.9% (0xc8d015b94a3Fb41DC13d6a9573bb454300023A94)
-    /// freeflow- 2.5% (0x5B588e36FF358D4376A76FB163fd69Da02A2A9a5)
 
     constructor(
         string memory _nonRevealedURI,
